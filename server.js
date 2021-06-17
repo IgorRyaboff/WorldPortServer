073d42c43ca48249e6127e74a2a5481221a5aa6f
Igor <mr.ryaboff2013@yandex.ru>
2021-06-17 23:50:39 +0600

    AliveCheck adjusting; configuring via config.json, users list

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ const fs = require('fs');
 const ArgsParser = require('node-args-parser');
 let args = ArgsParser(process.argv);
 
+let config = {
+    minPort: 200,
+    maxPort: 65535,
+    users: {
+        json: './users.json'
+    },
+    allowAnonymous: true,
+    aliveCheckInterval: 10000,
+    aliveCheckTimeout: 20000,
+    webSocketPort: 100
+};
+if (fs.existsSync('./config.json')) config = require('./config.json');
+
+let users = {};
+if (config.users && config.users.json && fs.existsSync(config.users.json)) users = require(config.users.json);
+
 function makeid(length) {
     var result = [];
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -45,9 +61,9 @@ class Session {
     }
 
     /**
-     * @type {Object<string, net.Socket>}
+     * @type {Object<string, [ net.Socket, net.Socket, Buffer[] ]>}
      */
-    tunnels = {};
+    sockets = {};
 
     /**
      * @type {websocket.connection}
@@ -60,6 +76,10 @@ class Session {
      * @type {net.Server}
      */
     #server;
+    /**
+     * @type {net.Server}
+     */
+    #s2Server;
 
     constructor(id, token, port, apiSocket) {
         console.log('new session yay', id, port);
@@ -67,32 +87,54 @@ class Session {
         this.token = token;
         this.externalPort = parseInt(port);
 
-        this.#server = net.createServer(s1 => {
+        this.#server = net.createServer(socket => {
             let id = makeid(2);
-            while (this.tunnels[id]) id = makeid(2);
-            console.log(`Incoming S1 #${id} in session ${this.id}`);
-            this.tunnels[id] = s1;
+            while (this.sockets[id]) id = makeid(2);
+            console.log(`Incoming socket ${id} in session ${this.id}`);
+            this.sockets[id] = [socket, false, []];
 
-            s1.on('data', chunk => {
-                if (!this.tunnels[id]) return;
-                this.sendBinary(id, chunk);
+            socket.on('data', chunk => {
+                if (!this.sockets[id]) return;
+                if (this.sockets[id][1]) this.sockets[id][1].write(chunk);
+                else this.sockets[id][2].push(chunk);
             });
 
-            s1.on('error', () => { });
-            s1.on('close', () => {
-                if (this.tunnels[id]) {
-                    this.send('tunnel.closed', {
-                        id: id
-                    });
-                    delete this.tunnels[id];
+            socket.on('error', () => { });
+            socket.on('close', () => {
+                if (this.sockets[id]) {
+                    if (this.sockets[id][1]) this.sockets[id][1].end();
+                    delete this.sockets[id];
                 }
             });
 
-            this.send('tunnel.created', {
+            this.send('s2.create', {
                 id: id
             });
         });
         this.#server.listen(this.externalPort);
+
+        this.#s2Server = net.createServer(socket => {
+            let id;
+            socket.on('data', chunk => {
+                if (id && this.sockets[id]) this.sockets[id][0].write(chunk);
+                else {
+                    id = chunk.toString();
+                    if (this.sockets[id]) {
+                        this.sockets[id][1] = socket;
+                        this.sockets[id][2].forEach(chunk => socket.write(chunk));
+                        console.log(`S2 created for socket ${id} in session ${this.id}`);
+                    }
+                    else socket.end();
+                }
+            });
+
+            socket.on('error', () => { });
+            socket.on('close', () => {
+                if (this.sockets[id]) this.sockets[id][0].end();
+                delete this.sockets[id];
+            });
+        });
+        this.#s2Server.listen(this.externalPort + 10000);
         this.initAPISocket(apiSocket);
     }
 
@@ -106,41 +148,28 @@ class Session {
         clearTimeout(this.#apiSocketReconTO);
         let lastAliveCheck = new Date;
         let aliveCheckInterval = setInterval(() => {
-            if (new Date - lastAliveCheck >= 20000) {
+            this.send('aliveCheck');
+        }, config.aliveCheckInterval);
+        let aliveCheckTimeoutChecker = setInterval(() => {
+            if (new Date - lastAliveCheck >= config.aliveCheckTimeout) {
                 console.log('Client did not aliveCheck last 20 seconds. Connection will be closed, session ' + this.id);
                 con.close();
             }
-            else this.send('aliveCheck');
-        }, 10000);
-        con.on('message', raw => {
-            if (raw.type == 'binary') {
-                let id = raw.binaryData.subarray(0, 2).toString();
-                let data = raw.binaryData.slice(2);
-                if (this.tunnels[id]) {
-                    this.tunnels[id].write(data);
-                    console.log(this.id, id, 'received binary');
-                }
-                else console.log('binary for unknown', this.id, id);
+        }, 1000);
+        con.on('message', msg => {
+            let data;
+            try {
+                data = JSON.parse(msg.utf8Data);
             }
-            else {
-                let msg;
-                try {
-                    msg = JSON.parse(raw.utf8Data);
-                }
-                catch (_e) { }
-    
-                if (!msg || !msg._e) return;
-                if (args.d) console.log(`${this.id}: ${msg._e}`);
-                switch (msg._e) {
-                    case 'aliveCheck': {
-                        if (args.d) console.log(this.id + ': aliveCheck recieved');
-                        lastAliveCheck = new Date;
-                        break;
-                    }
-                    case 'tunnel.close': {
-                        if (this.tunnels[msg.id]) this.tunnels[msg.id].end();
-                        break;
-                    }
+            catch (_e) { }
+
+            if (!data || !data._e) return;
+            if (args.d) console.log(`${this.id}: ${data._e}`);
+            switch (data._e) {
+                case 'aliveCheck': {
+                    if (args.d) console.log(this.id + ': aliveCheck recieved');
+                    lastAliveCheck = new Date;
+                    break;
                 }
             }
         });
@@ -154,42 +183,26 @@ class Session {
                 this.end();
             }, 10000);
             clearInterval(aliveCheckInterval);
+            clearInterval(aliveCheckTimeoutChecker);
         });
         this.#apiSocket = con;
-        this.#apiSocketBuffer.forEach(m => {
-            if (Buffer.isBuffer(m)) this.sendBinary(null, m);
-            else this.send(null, m);
-        });
+        this.#apiSocketBuffer.forEach(m => this.send(m._e, m));
         this.#apiSocketBuffer = [];
         this.send('session.created', {
             id: this.id,
             port: this.externalPort,
             token: this.token,
-            v: version
+            v: version,
+            aliveCheckInterval: config.aliveCheckInterval,
+            aliveCheckTimeout: config.aliveCheckTimeout
         });
     }
 
     send(e, data = {}) {
-        if (e) data._e = e;
+        data._e = e;
         if (!this.isOpened) return;
-        if (this.#apiSocket) {
-            this.#apiSocket.sendUTF(JSON.stringify(data));
-            console.log('sent ', data._e);
-        }
+        if (this.#apiSocket) this.#apiSocket.sendUTF(JSON.stringify(data));
         else this.#apiSocketBuffer.push(data);
-    }
-
-    sendBinary(tunnelID, data) {
-        if (!this.isOpened) return;
-
-        if (tunnelID) data = Buffer.from([...[...tunnelID].map(x => x.charCodeAt(0)), ...data]);
-        if (this.#apiSocket) {
-            this.#apiSocket.sendBytes(data);
-            console.log(this.id, tunnelID || data.subarray(0, 2).toString(), 'sent binary');
-            //console.assert([...data.subarray(0, 2).toString()].every(x => 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.indexOf(x) == -1), 'Invalid tunnel id ' + data.subarray(0, 2).toString());
-        }
-        else this.#apiSocketBuffer.push(data);
-
     }
 
     end() {
@@ -199,7 +212,8 @@ class Session {
         if (this.#apiSocket) this.#apiSocket.close(4001);
         this.#apiSocketBuffer = [];
         this.#server.close();
-        Object.values(this.tunnels).forEach(tun => tun.end());
+        this.#s2Server.close();
+        Object.values(this.sockets).forEach(pair => pair[0].end());
     }
 }
 
@@ -207,7 +221,7 @@ let apiServer = http.createServer((rq, rp) => {
     rp.writeHead(403);
     rp.end();
 });
-apiServer.listen(100);
+apiServer.listen(config.webSocketPort);
 
 let wsServer = new websocket.server({
     httpServer: apiServer,
@@ -234,17 +248,25 @@ wsServer.on('connect', con => {
         if (!data || !data._e) return;
         switch (data._e) {
             case 'session.create': {
+                if (data.authMethod == 'credentials') {
+                    if (data.login || !config.allowAnonymous) {
+                        if (!users[data.login]) return con.close(4008, '!Unknown login');
+                        if (users[data.login].password != data.password) return con.close(4008, '!Wrong password');
+                    }
+                }
+                else return con.close(4008, '!Unknown authMethod ' + data.authMethod);
+
                 if (Object.keys(sessions).length >= 45000) return con.close(4004);
                 let port = 0;
-                if (!isNaN(data.externalPort) && data.externalPort >= 10000 && data.externalPort <= 65535) {
-                    if (Object.values(sessions).some(s => data.externalPort == s.externalPort)) {
+                if (!isNaN(data.externalPort) && data.externalPort >= config.minPort && data.externalPort <= config.maxPort - 10000) {
+                    if (Object.values(sessions).some(s => data.externalPort == s.externalPort || data.externalPort == s.externalPort + 10000)) {
                         if (data.forceExternalPort) return con.close(4003);
                     }
                     else port = data.externalPort;
                 }
                 if (!port) {
-                    port = randomInteger(10000, 65535);
-                    while (Object.values(sessions).some(s => port == s.externalPort)) port = randomInteger(10000, 65535);
+                    port = randomInteger(config.minPort, config.maxPort - 10000);
+                    while (Object.values(sessions).some(s => port == s.externalPort || port == s.externalPort + 10000)) port = randomInteger(config.minPort, config.maxPort - 10000);
                 }
 
                 let id = makeid(3);
